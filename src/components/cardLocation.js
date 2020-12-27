090a72ff5204ce27eb31d2ab138abac789b7817a
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-27 15:48:59 +0100

    design of the card finished

--- src/components/cardLocation.js
+++ src/components/cardLocation.js
@@ -6,33 +6,35 @@ function CardLocation() {
 
     return (
         <div className="cardLocation">
-            <h2>Colocation 4 pers.</h2>
+            <h3 className="titleLoc">Colocation 4 pers.</h3>
 
-            <div className="tenant">
-                <img src="/avatar.jpg" alt="avatar pict" className="avatar" />
-                <div className="avatar-desc">
-                    <h4>Julien HOVELAQUE</h4>
-                    <p>CDI</p>
+            <div>
+                <div className="tenant">
+                    <img src="/avatar.jpg" alt="avatar pict" className="avatar" />
+                    <div className="avatar-desc">
+                        <p>H-C. HOVELAQUE</p>
+                        <p className="badgeJob">CDI</p>
+                    </div>
+                </div>
+
+                <div className="tenant">
+                    <img src="/avatar.jpg" alt="avatar pict" className="avatar" />
+                    <div className="avatar-desc">
+                        <p>J. HOVELAQUE</p>
+                        <p className="badgeJob">Profession libérale</p>
+                    </div>
                 </div>
             </div>
 
-            <div className="tenant">
-                <img src="/avatar.jpg" alt="avatar pict" className="avatar" />
-                <div className="avatar-desc">
-                    <h4>Julien HOVELAQUE</h4>
-                    <p>CDI</p>
-                </div>
-            </div>
-
-            <h3>Revenu mensuel moyen</h3>
+            <h6>Revenu mensuel moyen</h6>
             <div className="revenuBox">
                 <div className="candidate">
                     <p>Candidat</p>
-                    <p>5200 €</p>
+                    <p className="amount">5206 €</p>
                 </div>
                 <div className="candidate">
                     <p>Garant</p>
-                    <p>7000 €</p>
+                    <p className="amount">6700 €</p>
                 </div>
             </div>
 
